11e291d0f7d6645c6dd4c17d99048506c5740689
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-11 00:55:21 +0530

    11-03-2023

---
+++ RotateMatrix.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class RotateMatrix {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int li = 0;
+            int ri = arr[0].length - 1;
+            while (li < ri) {
+                int temp = arr[i][li];
+                arr[i][li] = arr[i][ri];
+                arr[i][ri] = temp;
+                li++;
+                ri--;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+
+    }
+}
